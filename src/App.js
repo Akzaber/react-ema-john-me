de1793d42c9d639876b21fbd3ed43aa5dc621c80
Akzaber <zaberkhan611@gmail.com>
2022-09-27 00:18:32 +0600

    main section in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './Component/Header/Header';
+import Main from './Component/Main/Main';
 function App() {
   return (
     <div>
       <Header></Header>
+      <Main></Main>
     </div>
   );
 }
---
+++ src/Component/Main/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Main.css'
+
+
+const Main = () => {
+  return (
+    <div className="main-shop">
+      <div className="shop-container">
+        <h1>Hello From shop container</h1>
+      </div>
+      <div className="cart-container">
+        <h3>Hello From cart-container</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
